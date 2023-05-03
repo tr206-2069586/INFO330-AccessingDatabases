0900a140b9ebdc2a38be438db2057c98ff2ce785
merp206 <tyler.merp@Tylers-MacBook-Air-4.local>
2023-05-02 19:53:33 -0700

    fin

--- Python/TeamAnalyzer.py
+++ Python/TeamAnalyzer.py
@@ -1,5 +1,6 @@
 import sqlite3  # This is the package for all sqlite3 access in Python
 import sys      # This helps with command-line parameters
+con = sqlite3.connect("../pokemon.sqlite")
 
 # All the "against" column suffixes:
 types = ["bug","dark","dragon","electric","fairy","fight",
@@ -17,11 +18,36 @@ for i, arg in enumerate(sys.argv):
         continue
 
     # Analyze the pokemon whose pokedex_number is in "arg"
-
+    weakness = []
+    strong = []
+    print("Analyzing " + arg)
     # You will need to write the SQL, extract the results, and compare
     # Remember to look at those "against_NNN" column values; greater than 1
     # means the Pokemon is strong against that type, and less than 1 means
     # the Pokemon is weak against that type
+    cursor = con.cursor()
+    if (arg.isnumeric()):  # This will store the name, type1 and type2 of the pokemone based on it's pokedexid
+        name_types = ("SELECT p.name, pv.type1, pv.type2 FROM pokemon p JOIN pokemon_types_view pv ON p.name = pv.name WHERE p.id = " + arg + ";")
+        cursor.execute(name_types)
+    elif (arg is not None): # This will store the name, type1 and type2 of the pokemone based on it's name
+        name_types = ('SELECT p.name, pv.type1, pv.type2 FROM pokemon p JOIN pokemon_types_view pv ON p.name = pv.name WHERE p.name = "' + arg + '";')
+        cursor.execute(name_types)
+    result = cursor.fetchone()
+
+    # This will pull the type and it's corresponding strength/weakness rating and then store it in the according array
+    for type in types:
+        sql = ("SELECT pv.against_" + type + " " +
+            "FROM pokemon_types_battle_view pv " +
+            "WHERE type1name = '" + result[1] + "' AND type2name = '" + result[2] +"';")
+        cursor.execute(sql)
+        test = cursor.fetchone()
+        if (test[0] > 1.0):
+            weakness.append(type)
+        elif (test[0] < 1.0):
+            strong.append(type)
+    # This will print the pokemone name type and it's strengths and weaknesses
+    print(result[0] + " (" + result[1] + " " + result[2] + ") " + "is strong against", strong ,"but is weak against", weakness)
+con.close()
 
 answer = input("Would you like to save this team? (Y)es or (N)o: ")
 if answer.upper() == "Y" or answer.upper() == "YES":
@@ -31,4 +57,3 @@ if answer.upper() == "Y" or answer.upper() == "YES":
     print("Saving " + teamName + " ...")
 else:
     print("Bye for now!")
-
